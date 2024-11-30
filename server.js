5b2d29317d0e34c7c9da0e565477f46eb36aa25f
Uijin Lee <100072451+dml113@users.noreply.github.com>
2024-11-30 15:12:26 +0900

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ io.on('connection', function (socket) {
 });
 
 var pool = new Pool({
-  connectionString: 'postgres://postgres:postgres@db.db/postgres'
+  connectionString: 'postgres://postgres:postgres@postgres.cde69tvxoswa.ap-northeast-2.rds.amazonaws.com/postgres'
 });
 
 async.retry(
